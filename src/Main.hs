cf93280e9189111929e63a17fa74a613e314e827
Hongchang Wu <wuhc85@gmail.com>
2017-01-16 02:22:24 -0500

    Add lookup benchmark

--- src/Main.hs
+++ src/Main.hs
@@ -14,15 +14,23 @@ import           Data.Map             (Map)
 import qualified Data.Map             as Map
 import           Test.QuickCheck      (generate, vector)
 
-type Input = ([Int], [Int])
+size :: Int
+size = 1000000
 
-generateInput :: Int -> IO Input
-generateInput k = (,) <$> return [1 .. k] <*> generate (vector k)
+type InsertInput = ([Int], [Int])
+
+generateInsertInput :: Int -> IO InsertInput
+generateInsertInput k = (,) <$> return [1 .. k] <*> generate (vector k)
+
+type LookupInput = [Int]
+
+generateLookupInput :: Int -> IO LookupInput
+generateLookupInput k = generate (vector k)
 
 main :: IO ()
 main =
   defaultMain
-    [ env (generateInput 1000000) $
+    [ env (generateInsertInput size) $
       \ ~(sorted, random) ->
          bgroup
            "insert"
@@ -69,6 +77,14 @@ main =
                    (insertHashTableIO random :: IO (LinearHashTable Int Int))
                ]
            ]
+    , env (generateLookupInput size) $
+      \ks ->
+         bgroup
+           "lookup"
+           [ bench "Data.Map" $ nf lookupMap ks
+           , bench "Data.IntMap" $ nf lookupIntMap ks
+           , bench "Data.HashMap.Lazy" $ nf lookupHashMap ks
+           ]
     ]
 
 insertMap :: [Int] -> Map Int Int
@@ -80,8 +96,28 @@ insertIntMap = foldl' (\m k -> IntMap.insert k 1 m) IntMap.empty
 insertHashMap :: [Int] -> HashMap Int Int
 insertHashMap = foldl' (\m k -> HashMap.insert k 1 m) HashMap.empty
 
-insertHashTableIO :: HashTable h => [Int] -> IO (IOHashTable h Int Int)
+insertHashTableIO
+  :: HashTable h
+  => [Int] -> IO (IOHashTable h Int Int)
 insertHashTableIO xs = do
-  m <- IOHashTable.new
-  mapM_ (\k -> IOHashTable.insert m k 1) xs
-  return m
+  ht <- IOHashTable.new
+  mapM_ (\k -> IOHashTable.insert ht k 1) xs
+  return ht
+
+aMap :: Map Int Int
+aMap = Map.fromAscList [(k, 1) | k <- [1 .. size]]
+
+lookupMap :: [Int] -> [Maybe Int]
+lookupMap = map (`Map.lookup` aMap)
+
+aIntMap :: IntMap Int
+aIntMap = IntMap.fromAscList [(k, 1) | k <- [1 .. size]]
+
+lookupIntMap :: [Int] -> [Maybe Int]
+lookupIntMap = map (`IntMap.lookup` aIntMap)
+
+aHashMap :: HashMap Int Int
+aHashMap = HashMap.fromList [(k, 1) | k <- [1 .. size]]
+
+lookupHashMap :: [Int] -> [Maybe Int]
+lookupHashMap = map (`HashMap.lookup` aHashMap)
